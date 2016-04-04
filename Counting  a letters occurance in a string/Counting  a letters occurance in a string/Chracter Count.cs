07c243c227e480b70b3ff422e4f2b96c632bc3f3
Maria <kanwal.shahid@ymail.com>
2016-04-04 23:05:26 +0500

    1

---
+++ Counting  a letters occurance in a string/Counting  a letters occurance in a string/Chracter Count.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counting__a_letters_occurance_in_a_string
+{
+    class CharacterCount
+    {
+        public static SortedDictionary<char, ulong> Count(string stringToCount)
+        {
+            SortedDictionary<char, ulong> characterCount = new SortedDictionary<char, ulong>();
+
+            foreach (var character in stringToCount)
+            {
+                if (!characterCount.ContainsKey(character))
+                {
+                    characterCount.Add(character, 1);
+                }
+                else
+                {
+                    characterCount[character]++;
+                }
+            }
+
+            return characterCount;
+        }
+    }
+}
+
---
+++ Counting  a letters occurance in a string/Counting  a letters occurance in a string/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counting__a_letters_occurance_in_a_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input;
+            Console.WriteLine("Enter Text for checking occurence of each letter in that string");
+            input = Console.ReadLine();
+
+            Console.WriteLine("Output");
+            var count = CharacterCount.Count(input);
+
+            foreach (var character in count)
+            {
+                Console.WriteLine("{0} is occuring {1} times", character.Key, character.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
