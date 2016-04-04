7b683fd9ed0e8dbb7af0152bbb4d8b06ff64c4d3
Maria <kanwal.shahid@ymail.com>
2016-04-04 23:15:46 +0500

    2

---
+++ Premutation( Show all combinations)/Premutation( Show all combinations)/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Premutation__Show_all_combinations_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input;
+            Console.WriteLine("Enter input to see all possible combinations");
+            input = Console.ReadLine();
+
+            Console.WriteLine("Outputs\n\n\n");
+            Shifting(input);
+            Console.ReadLine();
+        }
+        
+        public static void Shifting (string input)
+        {
+            Logic("", input);
+        }
+        private static void Logic(string temp, string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine(temp);
+                return;
+            }
+
+            else
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    string store= input.Substring(0, i);
+                    string store2=input.Substring(i + 1);
+                    string remaining = store + store2;
+
+                    Logic(temp + input[i], remaining);
+                }
+            }
+        }
+    }
+}
