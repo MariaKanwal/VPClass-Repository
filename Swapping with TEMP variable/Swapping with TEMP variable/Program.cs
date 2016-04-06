fb2df8c5bb3a04e4180ebebac182e16de666c50c
Maria <kanwal.shahid@ymail.com>
2016-04-06 21:26:09 +0500

    Swapping with TEMP variable.csproj

---
+++ Swapping with TEMP variable/Swapping with TEMP variable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapping_with_TEMP_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("Enter Value for Value A");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value for Value B");
+            b= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Value of a and b before sawapping");
+            Console.WriteLine();
+            Console.WriteLine("a=" + " " + a);
+            Console.WriteLine("b=" + " " + b);
+
+            int TEMP;
+            TEMP = a;
+            a = b;
+            b = TEMP;
+           
+            Console.WriteLine("Value of a and b after sawapping");
+            Console.WriteLine();
+            Console.WriteLine("a=" + " " + a);
+            Console.WriteLine("b=" + " " + b);
+            Console.ReadLine();  
+        }
+    }
+}
