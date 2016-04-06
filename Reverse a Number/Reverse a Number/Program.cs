375b98c38a5c6307b87004f8ebef9b990c147e2d
Maria <kanwal.shahid@ymail.com>
2016-04-06 21:33:37 +0500

    Reverse a Number.csproj

---
+++ Reverse a Number/Reverse a Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_a_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, reverse=0;
+            Console.WriteLine("Enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for(;num!=0;)
+            {
+                reverse = reverse * 10;
+                reverse = reverse + num% 10;
+                num = num / 10;
+
+            }
+            Console.WriteLine("Reversed Number is {0}",reverse);
+            Console.ReadLine();
+        }
+    }
+}
