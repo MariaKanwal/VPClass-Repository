d56967f55ab519d40f0cd81706cf8d04f1d24428
Maria <kanwal.shahid@ymail.com>
2016-04-06 21:25:47 +0500

    Range of Prime Numbers to specific Number.csproj

---
+++ Range of Prime Numbers to specific Number/Range of Prime Numbers to specific Number/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range_of_Prime_Numbers_to_specific_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNum, endNum;
+            int found = 0, count = 0;
+
+            Console.WriteLine("Enter Number for starting :  ");
+            startNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number for ending:");
+            endNum = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = startNum; i <= endNum; i++)
+            {
+                for (int j = 2; j <= Math.Pow(i, 2); j++)
+                {
+                    if (i % j == 0)
+                        count++;
+                }
+
+                if (count == 0 && i != 1)
+                {
+                    found++;
+                    Console.WriteLine("Prime Number {0}", i);
+                    count = 0;
+                }
+
+                count = 0;
+            }
+            Console.WriteLine("Total Prime Number Between Range {0} to {1} are", startNum, found);
+            //return 1;
+
+            Console.ReadLine();
+
+        }
+    }
+}
