2e9d2bc7cad9274258602da14e5d2331d0909d30
Maria <kanwal.shahid@ymail.com>
2016-04-06 22:41:47 +0500

    Factorial.csproj

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+
+        static int Fact(int n)
+        {
+            if (n <= 1)
+                return 1;
+            return n * Fact(n - 1);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number to find factorial: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int r = Fact(n);
+            Console.WriteLine(n + " FACTORIAL IS " + r);
+
+            Console.ReadLine();
+        }
+    }
+}
