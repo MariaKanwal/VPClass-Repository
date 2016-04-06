74eba6e9bb69b02b8e2cdb3cae01fa0166c21fca
Maria <kanwal.shahid@ymail.com>
2016-04-06 21:26:24 +0500

    Swapping without use of TEMP variable.csproj

---
+++ Swapping without use of TEMP variable/Swapping without use of TEMP variable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapping_without_use_of_TEMP_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("Enter Value for Value A");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value for Value B");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Value of a and b before sawapping");
+            Console.WriteLine();
+            Console.WriteLine("a=" + " " + a);
+            Console.WriteLine("b=" + " " + b);
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("Value of a and b after sawapping");
+            Console.WriteLine();
+            Console.WriteLine("a=" + " " + a);
+            Console.WriteLine("b=" + " " + b);
+            Console.ReadLine();  
+        }
+    }
+}
