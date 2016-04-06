1b7e0767f0d094d26d22f77fb3f90459944c29e7
Maria <kanwal.shahid@ymail.com>
2016-04-06 22:41:29 +0500

    Concept of Inheritance.csproj

---
+++ Concept of Inheritance/Concept of Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concept_of_Inheritance
+{
+    class inheritance : vehicle
+    {
+        public void Noise()
+        {
+            Console.WriteLine("All Vehicles Creates Noise !! ");
+        }
+
+        static void Main(string[] args)
+        {
+            inheritance obj = new inheritance();
+            obj.mode();
+            obj.feature();
+            obj.Noise();
+            Console.Read();
+        }
+    }
+    class Mode
+    {
+        public void mode()
+        {
+            Console.WriteLine("There are Many Modes of Transport !!");
+        }
+    }
+    class vehicle : Mode
+    {
+        public void feature()
+        {
+            Console.WriteLine("They Mainly Help in Travelling !!");
+        }
+    }
+}
+
