7cb10f7f26a61b70be1943aa498c1480f6804bca
Maria <kanwal.shahid@ymail.com>
2016-04-06 21:01:01 +0500

    Prime Number.csproj

---
+++ Prime Number/Prime Number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number, count = 0;
+            Console.WriteLine("ENTER NUMBER TO CHECK I IS PRIME OR NOT ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for (int a = 1; a <= number; a++)
+            {
+                if (number % a == 0)
+                {
+                    count++;
+                }
+            }
+
+            if (count == 2)
+            {
+                Console.WriteLine("\"{0}\" is a PRIME NUMBER \n", number);
+            }
+            else
+            {
+                Console.WriteLine("\"{0}\" is  NOT A PRIME NUMBER \n", number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
