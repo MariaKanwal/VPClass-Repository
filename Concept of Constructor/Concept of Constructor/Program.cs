0dbdd4bf92cc6d57cbd7980e0688ddacf6d39d09
Maria <kanwal.shahid@ymail.com>
2016-04-20 21:42:31 +0500

    Concept of Constructor.csproj

---
+++ Concept of Constructor/Concept of Constructor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concept_of_Constructor
+{
+    class Program
+    {
+        private double length;   
+        public Program(double len)  
+        {
+            Console.WriteLine("Object is being created, length = {0}", len);
+            length = len;
+        }
+
+        public void setLength(double len)
+        {
+            length = len;
+        }
+
+        public double getLength()
+        {
+            return length;
+        }
+
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+            Program line = new Program(num);
+            Console.WriteLine("Length of line : {0}", line.getLength());
+
+          
+            line.setLength(6.0);
+            Console.WriteLine("Length of line : {0}", line.getLength());
+            Console.ReadKey();
+        }
+    }
+}
+
