d486caec42b18358c630c34b9bbab3fb5faf796f
Maria <kanwal.shahid@ymail.com>
2016-05-16 17:28:06 +0500

    Decryption performed successfully now

--- Cryptography/Cryptography/Handler.cs
+++ Cryptography/Cryptography/Handler.cs
@@ -7,26 +7,21 @@ using System.Threading.Tasks;
 namespace Cryptography
 {
 
-    // Gives Error
     class Handler
     {
-
-        // Function Written To manage the # sign that was included in the Encrytion Of Image to increase Readability
-        public static byte[]  HASH_SIGN_HANDLER (string input_text)
+ // Function Written To manage the - sign that was included in the Encrytion Of Image to increase Readability
+        public static byte[] HASH_SIGN_HANDLER(string input_text)
         {
 
-
             //SPLIT function used to separate based on # Sign 
-            String[] Arr = input_text.Split('#');
+            String[] Arr = input_text.Split('-');
 
             // Byte Object Made to Take the Value becuase we are dealing in byte data 
             byte[] byte_object = new byte[Arr.Length];
 
 
-            for(int i = 0 ; i < Arr.Length ; i++)
+            for (int i = 0; i < Arr.Length; i++)
                 byte_object[i] = Convert.ToByte(Arr[i], 16);
-
-            
 
             //returning the value
             return byte_object;
